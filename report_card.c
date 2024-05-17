2a484c31dd4a845ab71b8307f92069ad814a296f
yjchoco <yeonje0815@gmail.com>
2024-05-17 16:23:47 +0900

    Revert "report card: Print grades of each subject"
    This reverts commit 90251371f7520c0bd56f02853b384a9025aa40ee.

--- report_card.c
+++ report_card.c
@@ -3,8 +3,11 @@
 void main(){
 	printf("This is Report Cards.c\n");
 	printf("what is the difference ? \n");
+<<<<<<< HEAD
 	printf("Math: 0 \n");
 	printf("English: 100 \n");
 	printf("Sum grade: 190 \n");
 	printf("Average grade: 95 \n");
+=======
+>>>>>>> parent of 9025137 (report card: Print grades of each subject)
 }
