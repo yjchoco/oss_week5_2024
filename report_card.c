e2a6e21d868055e0a2e8be36f032145e6c03f3e8
yjchoco <yeonje0815@gmail.com>
2024-05-17 16:25:31 +0900

    Revert "report card: math change (git commit --amend edit !!)"
    This reverts commit f2ecde3ed3fa902d12d0e1646c0c4567d212f862.

--- report_card.c
+++ report_card.c
@@ -4,10 +4,16 @@ void main(){
 	printf("This is Report Cards.c\n");
 	printf("what is the difference ? \n");
 <<<<<<< HEAD
+<<<<<<< HEAD
 	printf("Math: 0 \n");
 	printf("English: 100 \n");
 	printf("Sum grade: 190 \n");
 	printf("Average grade: 95 \n");
 =======
 >>>>>>> parent of 9025137 (report card: Print grades of each subject)
+=======
+	printf("Math: 90 \n");
+	printf("English: 100 \n");
+	printf("Sum grade: 190 \n");
+>>>>>>> parent of f2ecde3 (report card: math change (git commit --amend edit !!))
 }
